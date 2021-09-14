2e7ef01ca0d65b3ca6c8ff147e28135448a38747
Xilin Ding <dingxilin@web3.com>
2021-09-14 15:01:02 +0800

    chore:  @web3/vue-components- 0.1.0

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+TestEnv="test"
+PACKAGE_NAME=$(cat package.json \
+  | grep name \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+npm run build
+
+if [ -z "$1" ]; then
+npm version release
+fi
+if test $1 = $TestEnv 
+then
+   npm version patch
+fi
+
+PACKAGE_VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+git add .
+git commit -m "chore: $PACKAGE_NAME-$PACKAGE_VERSION"
+git tag "$PACKAGE_NAME-$PACKAGE_VERSION"
+
+git push origin --tags
